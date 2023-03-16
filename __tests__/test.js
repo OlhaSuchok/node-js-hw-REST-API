5360f873de7f2dd19f31b8eb228a9355ad4e4d68
Olha-Suchok <suchok_olya@ukr.net>
2023-03-16 16:01:36 +0200

    Create some test

---
+++ __tests__/test.js
@@ -0,0 +1,114 @@
+// =================================================================================
+// GetOneContactById middleware test
+// const { getOneContactById } = require("../models/contacts");
+// const Contact = require("../service/schemas/contacts");
+
+// describe("Contacts servise getOneContactById test", () => {
+//   it("Should return post data by providen ID", async () => {
+//     const mContactId = "1";
+//     const mUserId = "2";
+
+//     const contact = {
+//       _id: mContactId,
+//       name: "name",
+//       email: "email",
+//       phone: "phone",
+//       favorite: true,
+//       userId: mUserId,
+//     };
+
+//     jest.spyOn(Contact, "findOne").mockImplementationOnce(async () => contact);
+
+//     const result = await getOneContactById(mContactId, mUserId);
+
+//     expect(result._id).toEqual(mContactId);
+//     expect(result.name).toBeDefined();
+//     expect(result.email).toBeDefined();
+//     expect(result.phone).toBeDefined();
+//     expect(result.favorite).toBeDefined();
+//     expect(result.userId).toEqual(mUserId);
+//   });
+// });
+
+// =================================================================================
+// Auth middleware test
+// const jsonwebtoken = require("jsonwebtoken");
+// require("dotenv").config();
+// const { authMiddleware } = require("../middlewares/authMiddleware");
+// const { NotAuthorizedError } = require("../helpers/errors");
+
+// describe("Auth middleware test", () => {
+//   it("Should call next() and add user and token properties to req object", () => {
+//     const user = {
+//       _id: "1",
+//     };
+
+// const token = jsonwebtoken.sign(
+//   {
+//     _id: user._id,
+//   },
+//   process.env.JWT_SECRET,
+//   { expiresIn: "23h" }
+// );
+
+// const mReq = {
+//   headers: {
+//     authorization: `Bearer ${token}`,
+//   },
+// };
+// const mRes = {};
+// const mockNext = jest.fn();
+
+// authMiddleware(mReq, mRes, mockNext);
+
+// expect(mReq.token).toEqual(token);
+//     expect(mReq.user._id).toEqual(user._id);
+//     expect(mockNext).toHaveBeenCalled();
+//   });
+
+//   it("Should call next() with error in case authorization header is absent", () => {
+//     const mReq = {
+//       headers: {},
+//     };
+//     const mRes = {};
+//     const mockNext = jest.fn();
+
+//     authMiddleware(mReq, mRes, mockNext);
+
+//     expect(mockNext).toHaveBeenCalledWith(
+//       new NotAuthorizedError("Please, provade a token in request authorization")
+//     );
+//   });
+// });
+
+// =================================================================================
+// Simple test
+// const addTwoNumbers = (a, b) => {
+//   if (!a || !b) {
+//     throw new Error();
+//   }
+
+//   try {
+//     return a + b;
+//   } catch (error) {
+//     throw new Error("Please, provide valid numbers");
+//   }
+// };
+
+// describe("Addition service test", () => {
+//   it("Add two valid positive numbers", () => {
+//     const firstNumber = 1;
+//     const secondNumber = 1;
+//     const addTwoNumbersResult = addTwoNumbers(firstNumber, secondNumber);
+
+//     expect(addTwoNumbersResult).toEqual(firstNumber + secondNumber);
+//   });
+
+//   it("AddTwoNumbers returns error in case of invalid params", () => {
+//     const firstNumber = 4;
+
+//     expect(() => addTwoNumbers(firstNumber)).toThrow(
+//       "Please, provide valid numbers"
+//     );
+//   });
+// });
--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -4,21 +4,41 @@ const { NotAuthorizedError } = require("../helpers/errors");
 const User = require("../service/schemas/users");
 
 const authMiddleware = (req, res, next) => {
-  const [tokenType, token] = req.headers["authorization"].split(" ");
+  // const [tokenType, token] = req.headers["authorization"].split(" ");
 
-  if (tokenType !== "Bearer") {
-    next(new NotAuthorizedError("Unauthorized"));
-  }
+  // if (tokenType !== "Bearer") {
+  //   next(new NotAuthorizedError("Unauthorized"));
+  // }
 
-  if (!token) {
-    next(new NotAuthorizedError("Not authorized"));
-  }
+  // if (!token) {
+  //   next(new NotAuthorizedError("Unauthorized"));
+  // }
 
   try {
+    const { authorization } = req.headers;
+
+    if (!authorization) {
+      next(
+        new NotAuthorizedError(
+          "Please, provade a token in request authorization"
+        )
+      );
+    }
+
+    const [tokenType, token] = req.headers["authorization"].split(" ");
+
+    if (tokenType !== "Bearer") {
+      next(new NotAuthorizedError("Unauthorized"));
+    }
+
+    if (!token) {
+      next(new NotAuthorizedError("Unauthorized"));
+    }
+
     const user = jsonwebtoken.decode(token, process.env.JWT_SECRET);
 
     if (!user) {
-      next(new NotAuthorizedError("Not authorized"));
+      next(new NotAuthorizedError("Unauthorized"));
     }
 
     req.user = user;
@@ -27,6 +47,30 @@ const authMiddleware = (req, res, next) => {
   } catch (error) {
     next(new NotAuthorizedError("Invalid token"));
   }
+
+  // const [tokenType, token] = req.headers["authorization"].split(" ");
+
+  // if (tokenType !== "Bearer") {
+  //   next(new NotAuthorizedError("Unauthorized"));
+  // }
+
+  // if (!token) {
+  //   next(new NotAuthorizedError("Not authorized"));
+  // }
+
+  // try {
+  //   const user = jsonwebtoken.decode(token, process.env.JWT_SECRET);
+
+  //   if (!user) {
+  //     next(new NotAuthorizedError("Not authorized"));
+  //   }
+
+  //   req.user = user;
+  //   req.token = token;
+  //   next();
+  // } catch (error) {
+  //   next(new NotAuthorizedError("Invalid token"));
+  // }
 };
 
 module.exports = {
