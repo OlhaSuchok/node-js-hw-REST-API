7a1a9dd6610f38ee386fd32ba1de4ed12073d9af
Olha-Suchok <suchok_olya@ukr.net>
2023-03-16 16:05:29 +0200

    Fixed code

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -1,19 +1,8 @@
 const jsonwebtoken = require("jsonwebtoken");
 
 const { NotAuthorizedError } = require("../helpers/errors");
-const User = require("../service/schemas/users");
 
 const authMiddleware = (req, res, next) => {
-  // const [tokenType, token] = req.headers["authorization"].split(" ");
-
-  // if (tokenType !== "Bearer") {
-  //   next(new NotAuthorizedError("Unauthorized"));
-  // }
-
-  // if (!token) {
-  //   next(new NotAuthorizedError("Unauthorized"));
-  // }
-
   try {
     const { authorization } = req.headers;
 
@@ -47,30 +36,6 @@ const authMiddleware = (req, res, next) => {
   } catch (error) {
     next(new NotAuthorizedError("Invalid token"));
   }
-
-  // const [tokenType, token] = req.headers["authorization"].split(" ");
-
-  // if (tokenType !== "Bearer") {
-  //   next(new NotAuthorizedError("Unauthorized"));
-  // }
-
-  // if (!token) {
-  //   next(new NotAuthorizedError("Not authorized"));
-  // }
-
-  // try {
-  //   const user = jsonwebtoken.decode(token, process.env.JWT_SECRET);
-
-  //   if (!user) {
-  //     next(new NotAuthorizedError("Not authorized"));
-  //   }
-
-  //   req.user = user;
-  //   req.token = token;
-  //   next();
-  // } catch (error) {
-  //   next(new NotAuthorizedError("Invalid token"));
-  // }
 };
 
 module.exports = {
